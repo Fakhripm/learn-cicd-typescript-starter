197cc883be08d96d5f18b73d081a968d381215cf
Fakhripm <18221080@std.stei.itb.ac.id>
2026-02-05 18:25:14 +0700

    adding test coverage

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: "v8",
+      all: true,
+      include: ["src/**/*.ts"],
+      exclude: ["src/**/*.test.ts", "src/main.ts"],
+      reporter: ["text", "html"],
+    },
+  },
+});
